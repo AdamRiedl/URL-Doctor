48267d9d9c42cc59b5ebf6fc2f517544aa1b97b5
Adam Riedl <riedladam@seznam.cz>
2021-11-11 17:01:22 +0100

    Uprava indexu.php

---
+++ DataScrap/scraping/index.php
@@ -0,0 +1,35 @@
+<?php
+
+use Symfony\Component\DomCrawler\Crawler;
+
+
+require 'vendor/autoload.php';
+
+//$term = "Baroko";
+// URL
+$url = "https://www.plus500.com/cs/Instruments/CL";
+
+
+//i can use guzzle POST method if i must do some sort of FORM first
+$client = new \GuzzleHttp\Client();
+// go get data from URL
+$response = $client->request('GET',$url);
+$html = (string) $response->getBody();
+
+libxml_use_internal_errors(true);
+//print_r($html);
+
+$doc = new DOMDocument();
+$doc->loadHTML($html);
+
+$xpath = new DOMXPath($doc);
+
+$links = $xpath->evaluate('//div[@class="row"]//div[@class="small-12 medium-5 large-6 columns inst-details"]//div//p[@class="tittle-price"//span[@class="rate"]]');
+
+
+
+// do SOMETHING with the data
+
+
+
+
